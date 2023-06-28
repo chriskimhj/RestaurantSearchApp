bb8ed841b1a654f5e06dea9292cda2583fe97b17
Chris Kim <hjchristopher.dev@gmail.com>
2023-06-27 21:08:13 -0700

    Improve styling of results list and implement simple navigation

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
             <FlatList
                 horizontal
+                showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={result => result.id}
                 renderItem={({ item }) => {
-                    return <Text>{item.name}</Text>;
+                    return ( 
+                        <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+                            <ResultsDetail result={item}/>
+                        </TouchableOpacity>
+                    );
                 }}
             />
         </View>
@@ -21,7 +27,12 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 18,
         fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
     },
+    container: {
+        marginBottom: 10
+    }
 });
 
 export default ResultsList;
